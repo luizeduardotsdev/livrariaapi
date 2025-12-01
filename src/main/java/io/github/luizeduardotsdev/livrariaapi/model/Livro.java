5476a880cf32e08c85ef47092c21bcf908613ab8
luiz <luizeduardotsdev@gmail.com>
2025-12-01 15:49:54 -0300

    feat: remove o double e add BigDecimal no preco

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/model/Livro.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/model/Livro.java
@@ -3,6 +3,7 @@ package io.github.luizeduardotsdev.livrariaapi.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -30,7 +31,7 @@ public class Livro {
     private GeneroLivro genero;
 
     @Column(name = "preco", precision = 18, scale = 2)
-    private Double preco;
+    private BigDecimal preco;
 
     @ManyToOne
     @JoinColumn(name = "id_autor")
