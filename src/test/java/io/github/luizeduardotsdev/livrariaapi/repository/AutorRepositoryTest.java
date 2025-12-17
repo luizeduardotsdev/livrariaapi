5b9b2feadfee68152fa7b8459615bfeee7129963
luiz <luizeduardotsdev@gmail.com>
2025-12-17 20:46:25 -0300

    feat: cria um crud na classe de test de autor

--- src/test/java/io/github/luizeduardotsdev/livrariaapi/repository/AutorRepositoryTest.java
+++ src/test/java/io/github/luizeduardotsdev/livrariaapi/repository/AutorRepositoryTest.java
@@ -3,11 +3,13 @@ package io.github.luizeduardotsdev.livrariaapi.repository;
 import io.github.luizeduardotsdev.livrariaapi.model.Autor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.Optional;
+import java.util.UUID;
 
-@DataJpaTest
+@SpringBootTest
 public class AutorRepositoryTest {
 
     @Autowired
@@ -22,5 +24,33 @@ public class AutorRepositoryTest {
 
         var autorSalvo = autorRepository.save(autor);
         System.out.println(autor);
+    }
+
+    @Test
+    public void atualizarTest() {
+        var id = UUID.fromString("0x08237797703345F6BAA34957314F61AE");
+
+        Optional<Autor> possivelAutor = autorRepository.findById(id);
+
+        if (possivelAutor.isPresent()) {
+            Autor autorEncontrado = possivelAutor.get();
+            System.out.println("Dados do autor: " + autorEncontrado);
+
+            autorEncontrado.setDataNascimento(LocalDate.of(1988, 11, 1));
+
+            autorRepository.save(autorEncontrado);
+        }
+    }
+
+
+    @Test
+    public void countTest(){
+        System.out.println("Contagem de autores: " + autorRepository.count());
+    }
+
+    @Test
+    public void deletarAutorPorId() {
+        var id = UUID.fromString( "0x08237797703345F6BAA34957314F61AE");
+        autorRepository.deleteById(id);
     }
 }
