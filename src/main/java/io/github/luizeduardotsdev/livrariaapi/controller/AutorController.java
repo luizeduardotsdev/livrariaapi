7bb4db39961524f1d6ce9aeadcf9eb2cb8d3552e
luiz <luizeduardotsdev@gmail.com>
2025-12-21 11:33:15 -0300

    feat: injeta o service no controller

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/AutorController.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/AutorController.java
@@ -1,5 +1,6 @@
 package io.github.luizeduardotsdev.livrariaapi.controller;
 
+import io.github.luizeduardotsdev.livrariaapi.service.AutorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,10 +9,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/autores")
 public class AutorController {
 
+    private AutorService autorService;
+
+    public AutorController(AutorService autorService) {
+        this.autorService = autorService;
+    }
+
     @PostMapping
     public ResponseEntity salvar(@RequestBody AutorDTO autor) {
-
-
         return new ResponseEntity("autor salvo", HttpStatus.CREATED);
     }
 
