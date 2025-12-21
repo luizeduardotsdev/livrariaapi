e67412573212796ca07a5d06a13073a59163280d
luiz <luizeduardotsdev@gmail.com>
2025-12-21 14:57:46 -0300

    feat: add o metor de obterPorId no controller

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/AutorController.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/AutorController.java
@@ -1,11 +1,14 @@
 package io.github.luizeduardotsdev.livrariaapi.controller;
 
+import io.github.luizeduardotsdev.livrariaapi.model.Autor;
 import io.github.luizeduardotsdev.livrariaapi.service.AutorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/autores")
@@ -30,6 +33,20 @@ public class AutorController {
 
 
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<AutorDTO> obterDetalhes(@PathVariable("id") String id){
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = autorService.obterPorId(idAutor);
+
+        if (autorOptional.isPresent()){
+            Autor autor = autorOptional.get();
+            AutorDTO dto = new AutorDTO(autor.getId(), autor.getNome(), autor.getDataNascimento(), autor.getNacionalidade());
+            return ResponseEntity.ok(dto);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
 }
