2c55d034b24135ea3225551e57cfe26c7426fe3a
luiz <luizeduardotsdev@gmail.com>
2025-12-21 14:17:24 -0300

    feat: cria metodo mapper para autor no DTO

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/AutorDTO.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/AutorDTO.java
@@ -1,8 +1,20 @@
 package io.github.luizeduardotsdev.livrariaapi.controller;
 
+import io.github.luizeduardotsdev.livrariaapi.model.Autor;
+
 import java.time.LocalDate;
 
 public record AutorDTO(String nome,
                        LocalDate dataNascimento,
                        String Nacionalidade) {
+
+    public Autor mapearParaAutor(){
+        Autor autor = new Autor();
+        autor.setNome(this.nome);
+        autor.setDataNascimento(this.dataNascimento);
+        autor.setNacionalidade(this.Nacionalidade);
+
+        return autor;
+    }
+
 }
