6267183d5e899bcb17ce27833ce796bf2b58c6b7
luiz <luizeduardotsdev@gmail.com>
2025-12-21 06:35:10 -0300

    faet: implementa findByTitulo no repository

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/repository/LivroRepository.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/repository/LivroRepository.java
@@ -3,7 +3,11 @@ package io.github.luizeduardotsdev.livrariaapi.repository;
 import io.github.luizeduardotsdev.livrariaapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface LivroRepository extends JpaRepository<Livro, UUID> {
+
+
+    List<Livro> findByTitulo(String titulo);
 }
