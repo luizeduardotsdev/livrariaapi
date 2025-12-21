727e04dee38c252106cd7a25219112413d1630cc
luiz <luizeduardotsdev@gmail.com>
2025-12-21 06:55:39 -0300

    feat: add metodos no repository livro

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/repository/LivroRepository.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/repository/LivroRepository.java
@@ -3,11 +3,16 @@ package io.github.luizeduardotsdev.livrariaapi.repository;
 import io.github.luizeduardotsdev.livrariaapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 public interface LivroRepository extends JpaRepository<Livro, UUID> {
 
 
+    // select * from livro where livro = livro
     List<Livro> findByTitulo(String titulo);
+
+    // select * from livro where data_publicacao between ? and ?
+    List<Livro> findByDataPublicacaoBetween(LocalDate inicio, LocalDate fim);
 }
