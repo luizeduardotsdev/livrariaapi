56be5f1e7b8c0fa3c0734d03b04389213e1b3b19
luiz <luizeduardotsdev@gmail.com>
2025-12-22 07:42:45 -0300

    feat: implementa o metodo pesquisar no controller

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/AutorController.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/AutorController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/autores")
@@ -61,6 +63,22 @@ public class AutorController {
 
         autorService.deletar(autorOptional.get());
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> pesquisar(
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "nacionalidade", required = false) String nacionalidade) {
+
+        List<Autor> resultado = autorService.pesquisar(nome, nacionalidade);
+        List<AutorDTO> lista = resultado.stream().map(autor -> new AutorDTO(
+                autor.getId(),
+                autor.getNome(),
+                autor.getDataNascimento(),
+                autor.getNacionalidade())).
+                collect(Collectors.toList());
+
+        return ResponseEntity.ok(lista);
     }
 
 }
