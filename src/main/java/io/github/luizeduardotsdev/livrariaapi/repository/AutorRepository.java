b32c5b1f2ba9c51a50be022a7987e1a35ab5a005
luiz <luizeduardotsdev@gmail.com>
2025-12-25 07:56:20 -0300

    feat: cria o metodo de de busca por nome, nacionalidade e dtnascimento

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/repository/AutorRepository.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/repository/AutorRepository.java
@@ -3,7 +3,9 @@ package io.github.luizeduardotsdev.livrariaapi.repository;
 import io.github.luizeduardotsdev.livrariaapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
@@ -11,4 +13,6 @@ public interface AutorRepository extends JpaRepository<Autor, UUID> {
     List<Autor> findByNome(String nome);
     List<Autor> findByNacionalidade(String nacionalidade);
     List<Autor> findByNomeAndNacionalidade(String nome, String nacionalidade);
+
+    Optional<Autor> findByNomeAndDataNascimentoAndNacionalidade(String nome, LocalDate dataNascimento, String nacionalidade);
 }
