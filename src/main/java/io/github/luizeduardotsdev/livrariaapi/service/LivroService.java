c407f74be7e356e7b30751be0852a34d92be76e4
luiz <luizeduardotsdev@gmail.com>
2025-12-25 12:24:32 -0300

    feat: cria o service e livro e injeta dependecia do repository

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/service/LivroService.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/service/LivroService.java
@@ -1,4 +1,13 @@
 package io.github.luizeduardotsdev.livrariaapi.service;
 
+import io.github.luizeduardotsdev.livrariaapi.repository.LivroRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LivroService {
+    private final LivroRepository livroRepository;
+
+    public LivroService(LivroRepository livroRepository) {
+        this.livroRepository = livroRepository;
+    }
 }
