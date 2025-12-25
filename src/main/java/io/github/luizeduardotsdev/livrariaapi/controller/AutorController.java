c91cb83a2d57462924b06d443cb3ea296d3425cd
luiz <luizeduardotsdev@gmail.com>
2025-12-25 10:50:10 -0300

    feat: add o metodo de pesquisa dinamica no controller

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/AutorController.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/AutorController.java
@@ -83,7 +83,7 @@ public class AutorController {
             @RequestParam(value = "nome", required = false) String nome,
             @RequestParam(value = "nacionalidade", required = false) String nacionalidade) {
 
-        List<Autor> resultado = autorService.pesquisar(nome, nacionalidade);
+        List<Autor> resultado = autorService.pesquisaByExample(nome, nacionalidade);
         List<AutorDTO> lista = resultado.stream().map(autor -> new AutorDTO(
                 autor.getId(),
                 autor.getNome(),
