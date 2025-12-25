2a767d960030fb41a67cc7281e3d92c9562dc5b3
luiz <luizeduardotsdev@gmail.com>
2025-12-25 10:49:42 -0300

    feat: cria metodo pesquisaByExample para fazer pesquisa dinamica

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/service/AutorService.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/service/AutorService.java
@@ -5,6 +5,8 @@ import io.github.luizeduardotsdev.livrariaapi.model.Autor;
 import io.github.luizeduardotsdev.livrariaapi.repository.AutorRepository;
 import io.github.luizeduardotsdev.livrariaapi.repository.LivroRepository;
 import io.github.luizeduardotsdev.livrariaapi.validator.AutorValidator;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -62,6 +64,22 @@ public class AutorService {
         }
 
         return autorRepository.findAll();
+    }
+
+    public List<Autor> pesquisaByExample(String nome, String nacionalidade) {
+        var autor = new Autor();
+        autor.setNome(nome);
+        autor.setNacionalidade(nacionalidade);
+
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreNullValues()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example<Autor> autorExample = Example.of(autor, matcher);
+
+        return autorRepository.findAll(autorExample);
+
     }
 
     public boolean possuiLivro(Autor autor) {
