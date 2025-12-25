9e2af9ddc3e4a572913b0517ba9677456688b4e7
luiz <luizeduardotsdev@gmail.com>
2025-12-25 07:57:17 -0300

    feat: cria o metodo de validacao se o autor ja esta cadastrado

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/validator/AutorValidator.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/validator/AutorValidator.java
@@ -1,8 +1,11 @@
 package io.github.luizeduardotsdev.livrariaapi.validator;
 
+import io.github.luizeduardotsdev.livrariaapi.exceptions.RegistroDuplicadoException;
 import io.github.luizeduardotsdev.livrariaapi.model.Autor;
 import io.github.luizeduardotsdev.livrariaapi.repository.AutorRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class AutorValidator {
@@ -14,6 +17,20 @@ public class AutorValidator {
     }
 
     public void validar(Autor autor) {
-        
+        if (existeAutorCadastrado(autor)) {
+            throw new RegistroDuplicadoException("autor ja cadastrado");
+        }
+    }
+
+    private boolean existeAutorCadastrado(Autor autor) {
+        Optional<Autor> autorEncontrado = autorRepository.findByNomeAndDataNascimentoAndNacionalidade(
+                autor.getNome(),autor.getDataNascimento(), autor.getNacionalidade());
+
+        if (autor.getId() == null) {
+            return autorEncontrado.isPresent();
+        }
+
+        return !autor.getId().equals(autorEncontrado.get().getId()) && autorEncontrado.isPresent();
+
     }
 }
