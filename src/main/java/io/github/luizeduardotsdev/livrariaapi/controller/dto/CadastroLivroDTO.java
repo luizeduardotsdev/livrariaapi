af07e236f89b5bd2bddbeb04bd5bf8b36fe3153d
luiz <luizeduardotsdev@gmail.com>
2025-12-25 12:31:39 -0300

    feat: mapeia os DTOs de cadastro e pesquisa

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/dto/CadastroLivroDTO.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/dto/CadastroLivroDTO.java
@@ -6,14 +6,11 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record LivroDTO(
-
+public record CadastroLivroDTO(
         String isbn,
         String titulo,
         LocalDate dataPublicacao,
         GeneroLivro genero,
         BigDecimal preco,
-        UUID idAutor
-        
-        ) {
+        UUID idAutor) {
 }
--- src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/dto/ResultadoPesquisaLivroDTO.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/dto/ResultadoPesquisaLivroDTO.java
@@ -1,4 +1,18 @@
 package io.github.luizeduardotsdev.livrariaapi.controller.dto;
 
-public record ResultadoPesquisaLivroDTO() {
+import io.github.luizeduardotsdev.livrariaapi.model.GeneroLivro;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record ResultadoPesquisaLivroDTO(
+        UUID id,
+        String isbn,
+        String titulo,
+        LocalDate dataPublicacao,
+        GeneroLivro genero,
+        BigDecimal preco,
+        AutorDTO autor
+) {
 }
