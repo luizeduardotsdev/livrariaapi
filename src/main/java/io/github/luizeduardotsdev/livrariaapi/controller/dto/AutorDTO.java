85e6dcbe01689f384082c9d551263ca10de60ba1
luiz <luizeduardotsdev@gmail.com>
2025-12-25 14:02:02 -0300

    refactor: remove o metodo de mapear do dto

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/dto/AutorDTO.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/dto/AutorDTO.java
@@ -1,6 +1,5 @@
 package io.github.luizeduardotsdev.livrariaapi.controller.dto;
 
-import io.github.luizeduardotsdev.livrariaapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
@@ -23,14 +22,4 @@ public record AutorDTO(
         @NotBlank(message = "campo obrigatorio")
         @Size(min = 2, max = 50, message = "campo fora do tamanho padrao")
         String nacionalidade) {
-
-    public Autor mapearParaAutor(){
-        Autor autor = new Autor();
-        autor.setNome(this.nome);
-        autor.setDataNascimento(this.dataNascimento);
-        autor.setNacionalidade(this.nacionalidade);
-
-        return autor;
-    }
-
 }
