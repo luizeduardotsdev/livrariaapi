fe0d0818b1d7dcb541591407d41a92d676239ea7
luiz <luizeduardotsdev@gmail.com>
2025-12-25 12:25:12 -0300

    feat: cria o controller de livros e injeta dependecia de service

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/LivroController.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/LivroController.java
@@ -1,4 +1,16 @@
 package io.github.luizeduardotsdev.livrariaapi.controller;
 
+import io.github.luizeduardotsdev.livrariaapi.service.LivroService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/livros")
 public class LivroController {
+
+    private final LivroService livroService;
+
+    public LivroController(LivroService livroService) {
+        this.livroService = livroService;
+    }
 }
