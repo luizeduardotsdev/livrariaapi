fa3d2e70961f693448d0041e69887f7c8e21133c
luiz <luizeduardotsdev@gmail.com>
2025-12-25 19:22:21 -0300

    feat: cria interface para gerar uri para mostrar onde esta sendo salvo

---
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/UriController.java
@@ -0,0 +1,18 @@
+package io.github.luizeduardotsdev.livrariaapi;
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.UUID;
+
+public interface UriController {
+
+    default URI gerarHeaderLocation(UUID id) {
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(id)
+                .toUri();
+    }
+
+}
