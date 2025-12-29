2b69370fed9784d035ab6046078d150f22515a45
luiz <luizeduardotsdev@gmail.com>
2025-12-29 08:48:46 -0300

    feat: extende JpaSpecification para busca no livro repository

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/repository/LivroRepository.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/repository/LivroRepository.java
@@ -3,12 +3,13 @@ package io.github.luizeduardotsdev.livrariaapi.repository;
 import io.github.luizeduardotsdev.livrariaapi.model.Autor;
 import io.github.luizeduardotsdev.livrariaapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
-public interface LivroRepository extends JpaRepository<Livro, UUID> {
+public interface LivroRepository extends JpaRepository<Livro, UUID>, JpaSpecificationExecutor<Livro> {
 
 
     // select * from livro where livro = livro
