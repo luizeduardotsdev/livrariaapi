983c248a7e01d3087fe1c1152c50c43515464345
luiz <luizeduardotsdev@gmail.com>
2025-12-30 11:17:49 -0300

    feat: cria validacao de preco no validator de livro

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/validator/LivroValidator.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/validator/LivroValidator.java
@@ -1,5 +1,6 @@
 package io.github.luizeduardotsdev.livrariaapi.validator;
 
+import io.github.luizeduardotsdev.livrariaapi.exceptions.CampoInvalidoException;
 import io.github.luizeduardotsdev.livrariaapi.exceptions.RegistroDuplicadoException;
 import io.github.luizeduardotsdev.livrariaapi.model.Livro;
 import io.github.luizeduardotsdev.livrariaapi.repository.LivroRepository;
@@ -9,6 +10,8 @@ import java.util.Optional;
 
 @Component
 public class LivroValidator {
+
+    public static final int ANO_EXIGENCIA_PRECO = 2020;
 
     private LivroRepository livroRepository;
 
@@ -20,6 +23,15 @@ public class LivroValidator {
         if (existeLivroComIsbn(livro)) {
             throw new RegistroDuplicadoException("ISBN já cadastrado!");
         }
+
+        if (isPrecoObrigatorioNulo(livro)) {
+            throw new CampoInvalidoException("preco", "Para livros com ano de publicação a partir de 2020, o preço é obrigatorio");
+        }
+    }
+
+    private boolean isPrecoObrigatorioNulo(Livro livro) {
+
+        return livro.getPreco() == null && livro.getDataPublicacao().getYear() >= ANO_EXIGENCIA_PRECO;
     }
 
     private boolean existeLivroComIsbn(Livro livro) {
