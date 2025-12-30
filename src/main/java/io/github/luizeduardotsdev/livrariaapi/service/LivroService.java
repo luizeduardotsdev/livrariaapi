faf9e05f235da736b5c69311ac2ef878c9942e72
luiz <luizeduardotsdev@gmail.com>
2025-12-30 09:13:09 -0300

    feat: implementa metodo de pesquisa na camada de service usando specs

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/service/LivroService.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/service/LivroService.java
@@ -1,11 +1,17 @@
 package io.github.luizeduardotsdev.livrariaapi.service;
 
+import io.github.luizeduardotsdev.livrariaapi.model.GeneroLivro;
 import io.github.luizeduardotsdev.livrariaapi.model.Livro;
 import io.github.luizeduardotsdev.livrariaapi.repository.LivroRepository;
+import io.github.luizeduardotsdev.livrariaapi.repository.specs.LivroSpecs;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import static io.github.luizeduardotsdev.livrariaapi.repository.specs.LivroSpecs.*;
 
 @Service
 public class LivroService {
@@ -25,5 +31,31 @@ public class LivroService {
 
     public void deletar(Livro livro) {
         livroRepository.delete(livro);
+    }
+
+    public List<Livro> pesquisa(String isbn,String titulo, String nomeAutor, GeneroLivro genero, Integer anoPublicacao) {
+        Specification<Livro> specs = Specification.where(((root, query, criteriaBuilder) -> criteriaBuilder.conjunction()));
+
+        if (isbn != null) {
+            specs = specs.and(isbnEqual(isbn));
+        }
+
+        if (titulo != null) {
+            specs = specs.and(tituloLike(titulo));
+        }
+
+        if (genero != null) {
+            specs = specs.and(generoEqual(genero));
+        }
+
+        if (anoPublicacao != null) {
+            specs = specs.and(anoPublicacaoEqual(anoPublicacao));
+        }
+
+        if (nomeAutor != null) {
+            specs = specs.and(nomeAutorLike(nomeAutor));
+        }
+
+        return livroRepository.findAll(specs);
     }
 }
