ea6de2b145210f7f2267edf89e21a0abee398990
luiz <luizeduardotsdev@gmail.com>
2025-12-30 10:30:37 -0300

    feat: cria classe validator para livro e faz validacao de isbn

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/validator/LivroValidator.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/validator/LivroValidator.java
@@ -1,4 +1,37 @@
 package io.github.luizeduardotsdev.livrariaapi.validator;
 
+import io.github.luizeduardotsdev.livrariaapi.exceptions.RegistroDuplicadoException;
+import io.github.luizeduardotsdev.livrariaapi.model.Livro;
+import io.github.luizeduardotsdev.livrariaapi.repository.LivroRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
 public class LivroValidator {
+
+    private LivroRepository livroRepository;
+
+    public LivroValidator(LivroRepository livroRepository) {
+        this.livroRepository = livroRepository;
+    }
+
+    public void validarLivro(Livro livro) {
+        if (existeLivroComIsbn(livro)) {
+            throw new RegistroDuplicadoException("ISBN já cadastrado!");
+        }
+    }
+
+    private boolean existeLivroComIsbn(Livro livro) {
+        Optional<Livro> livroEncotrado = livroRepository.findByIsbn(livro.getIsbn());
+
+        if (livro.getId() == null) {
+            return livroEncotrado.isPresent();
+        }
+
+        return livroEncotrado
+                .map(Livro::getId)
+                .stream()
+                .anyMatch(id -> !id.equals(livro.getId()));
+    }
 }
