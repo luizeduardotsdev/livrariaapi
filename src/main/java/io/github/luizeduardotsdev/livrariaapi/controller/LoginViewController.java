6bc676a23381adf70ce171fac92ac9db98ec942b
luiz <luizeduardotsdev@gmail.com>
2026-01-06 19:25:52 -0300

    feat: cria viewcontroller de login

---
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/LoginViewController.java
@@ -0,0 +1,24 @@
+package io.github.luizeduardotsdev.livrariaapi.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginViewController {
+
+
+    @GetMapping("/login")
+    public String paginaLogin() {
+        return "login";
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    public String paginaHome(Authentication auth){
+        return "Olá " + auth.getName();
+    }
+
+
+}
