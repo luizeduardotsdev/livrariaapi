136f46875016d21636c0366d2f223e67cf5607ea
luiz <luizeduardotsdev@gmail.com>
2026-01-06 11:03:28 -0300

    feat: cria o usuario service implementa repository e o password enconder, criando metodo de salvar e obterPorLogin

---
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/service/UsuarioService.java
@@ -0,0 +1,29 @@
+package io.github.luizeduardotsdev.livrariaapi.service;
+
+import io.github.luizeduardotsdev.livrariaapi.model.Usuario;
+import io.github.luizeduardotsdev.livrariaapi.repository.UsuarioRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
+        this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void salvar(Usuario usuario) {
+        var senha = usuario.getSenha();
+        usuario.setSenha(passwordEncoder.encode(senha));
+
+        usuarioRepository.save(usuario);
+    }
+
+    public Usuario obetPorLogin(String login) {
+        return usuarioRepository.findByLogin(login);
+    }
+}
