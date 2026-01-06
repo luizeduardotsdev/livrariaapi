17aa9e9d7050b1c96451891154de3b84dc79a359
luiz <luizeduardotsdev@gmail.com>
2026-01-06 11:01:17 -0300

    feat: cria entidade usuario para login

---
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/model/Usuario.java
@@ -0,0 +1,32 @@
+package io.github.luizeduardotsdev.livrariaapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.JdbcTypeCode;
+
+import java.sql.Types;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "usuario")
+@Data
+public class Usuario {
+
+    @Id
+    @Column(name = "id")
+    @JdbcTypeCode(Types.VARCHAR)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "login", nullable = false, unique = true, length = 20)
+    private String login;
+
+    @Column(name = "senha", nullable = false, length = 300)
+    private String senha;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "usuario_roles", joinColumns = @JoinColumn(name = "usuario_id"))
+    @Column(name = "roles")
+    private List<String> roles;
+}
