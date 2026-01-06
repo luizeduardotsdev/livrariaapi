3b5ffa0cdd03c217457871a962878403f1697699
luiz <luizeduardotsdev@gmail.com>
2026-01-06 19:55:13 -0300

    feat: cria metodo para encontrar usuario logado

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/security/CustomAuthenticationProvider.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/security/CustomAuthenticationProvider.java
@@ -1,14 +1,48 @@
 package io.github.luizeduardotsdev.livrariaapi.security;
 
+import io.github.luizeduardotsdev.livrariaapi.model.Usuario;
+import io.github.luizeduardotsdev.livrariaapi.service.UsuarioService;
+import lombok.Data;
+import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
+@Component
+@Data
 public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final UsuarioService usuarioService;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public @Nullable Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+        String login = authentication.getName();
+        String senhaDigitada = authentication.getCredentials().toString();
+
+        Usuario usuarioEncontrado = usuarioService.obetPorLogin(login);
+
+        if (usuarioEncontrado == null) {
+            throw getErroUsuarioNaoEncontrado();
+        }
+
+        String senhaCriptografada = usuarioEncontrado.getSenha();
+
+        boolean senhasIguais = passwordEncoder.matches(senhaDigitada, senhaCriptografada);
+
+        if (senhasIguais) {
+            return new CustomAuthentication(usuarioEncontrado);
+        }
+
+        throw getErroUsuarioNaoEncontrado();
+    }
+
+    private static @NonNull UsernameNotFoundException getErroUsuarioNaoEncontrado() {
+        return new UsernameNotFoundException("Usuario ou senha incorretos!");
     }
 
     @Override
