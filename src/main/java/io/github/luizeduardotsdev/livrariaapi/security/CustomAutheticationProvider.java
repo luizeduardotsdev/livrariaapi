1a4db60b0136a7d30956f418a65a5875c43d9176
luiz <luizeduardotsdev@gmail.com>
2026-01-06 19:37:45 -0300

    feat: cria classe CustomAutheticationProvider e extende AuthenticationProvider implementando seus metodos

---
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/security/CustomAutheticationProvider.java
@@ -0,0 +1,18 @@
+package io.github.luizeduardotsdev.livrariaapi.security;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+public class CustomAutheticationProvider implements AuthenticationProvider {
+    @Override
+    public @Nullable Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
