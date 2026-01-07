92b7594a10c5163857fea532d24b7784935ca549
luiz <luizeduardotsdev@gmail.com>
2026-01-07 09:39:05 -0300

    feat: transforma usuario do google em um CustomAuthtentication success handler

---
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/security/LoginSocialSuccessHandler.java
@@ -0,0 +1,43 @@
+package io.github.luizeduardotsdev.livrariaapi.security;
+
+import io.github.luizeduardotsdev.livrariaapi.model.Usuario;
+import io.github.luizeduardotsdev.livrariaapi.service.UsuarioService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Data;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Data
+public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    private final UsuarioService usuarioService;
+
+    @Override
+    public void onAuthenticationSuccess(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication) throws ServletException, IOException {
+
+        OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+        OAuth2User oAuth2USer = oAuth2AuthenticationToken.getPrincipal();
+
+        String email = oAuth2USer.getAttribute("email");
+
+        Usuario usuario = usuarioService.obterPorEmail(email);
+
+        authentication = new CustomAuthentication(usuario);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        super.onAuthenticationSuccess(request, response, authentication);
+    }
+}
