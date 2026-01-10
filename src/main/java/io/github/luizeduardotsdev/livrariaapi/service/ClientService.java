d4197ff55ef99f4199d1cda2567f63dc89e564b5
luiz <luizeduardotsdev@gmail.com>
2026-01-10 11:25:01 -0300

    feat: implementa password encoder no service

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/service/ClientService.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/service/ClientService.java
@@ -2,20 +2,23 @@ package io.github.luizeduardotsdev.livrariaapi.service;
 
 import io.github.luizeduardotsdev.livrariaapi.model.Client;
 import io.github.luizeduardotsdev.livrariaapi.repository.ClientRespository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 public class ClientService {
 
     private final ClientRespository clientRespository;
+    private final PasswordEncoder passwordEncoder;
 
-    public ClientService(ClientRespository clientRespository) {
+    public ClientService(ClientRespository clientRespository, PasswordEncoder passwordEncoder) {
         this.clientRespository = clientRespository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Client salvar(Client client){
+        var senhaCriptograda = passwordEncoder.encode(client.getClientSecret());
+        client.setClientSecret(senhaCriptograda);
         return clientRespository.save(client);
     }
 
