5a9beb9cab538a0776ee12e56cb0ccb64c1c0b3b
luiz <luizeduardotsdev@gmail.com>
2026-01-10 11:58:12 -0300

    style: remove linha inuteis

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/config/SecurityConfiguration.java
@@ -1,8 +1,6 @@
 package io.github.luizeduardotsdev.livrariaapi.config;
 
-import io.github.luizeduardotsdev.livrariaapi.security.CustomUserDetailsService;
 import io.github.luizeduardotsdev.livrariaapi.security.LoginSocialSuccessHandler;
-import io.github.luizeduardotsdev.livrariaapi.service.UsuarioService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,9 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -35,31 +30,6 @@ public class SecurityConfiguration {
                     oauth2.successHandler(successHandler);
                         })
                 .build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-//    @Bean
-    public UserDetailsService userDetailsService(UsuarioService usuarioService) {
-//        UserDetails user1 = User.builder()
-//                .username("user")
-//                .password(encoder.encode("password"))
-//                .roles("USER")
-//                .build();
-//
-//        UserDetails user2 = User.builder()
-//                .username("admin")
-//                .password(encoder.encode("admin"))
-//                .roles("ADMIN")
-//                .build();
-//
-//
-//        return new InMemoryUserDetailsManager(user1, user2);
-
-        return new CustomUserDetailsService(usuarioService);
     }
 
     @Bean
