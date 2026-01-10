cda27b72410845c5af7bde619f7442b1e8e7ebb9
luiz <luizeduardotsdev@gmail.com>
2026-01-10 10:29:55 -0300

    feat: add annotations collumn com regras de negocio

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/model/Client.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/model/Client.java
@@ -2,24 +2,32 @@ package io.github.luizeduardotsdev.livrariaapi.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.JdbcTypeCode;
 
+import java.sql.Types;
 import java.util.UUID;
 
 @Entity
-@Table
+@Table(name = "client")
 @Data
 public class Client {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @JdbcTypeCode(Types.VARCHAR)
+    @Column(name = "id")
     private UUID id;
 
+    @Column(name = "client_id", nullable = false)
     private String clientId;
 
+    @Column(name = "client_secret", nullable = false)
     private String clientSecret;
 
+    @Column(name = "redirect_uri", nullable = false)
     private String redirectUri;
 
+    @Column(name = "redirect_uri", nullable = false)
     private String scope;
 
 }
