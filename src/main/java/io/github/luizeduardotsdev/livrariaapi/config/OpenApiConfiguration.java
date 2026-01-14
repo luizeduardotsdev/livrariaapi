adba5267fa3e2013b61ff07a560ac421ecceb2cb
luiz <luizeduardotsdev@gmail.com>
2026-01-14 11:45:09 -0300

    feat: cria classe configuration para o open api

---
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/config/OpenApiConfiguration.java
@@ -0,0 +1,21 @@
+package io.github.luizeduardotsdev.livrariaapi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Library API",
+                version = "v1",
+                contact = @Contact(
+                        name = "luiz eduardo",
+                        email = "luizeduardotsdev@gmail.com"
+                )
+
+        )
+)
+public class OpenApiConfiguration {
+}
