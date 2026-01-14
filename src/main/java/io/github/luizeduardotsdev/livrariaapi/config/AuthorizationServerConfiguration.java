6f6dc551f2c4afa5d83b3520dd1ac5e68087d2f6
luiz <luizeduardotsdev@gmail.com>
2026-01-14 08:47:23 -0300

    feat: cria bean para customizacao de token e configura para retornar o email e as authorities

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/config/AuthorizationServerConfiguration.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/config/AuthorizationServerConfiguration.java
@@ -5,6 +5,7 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import io.github.luizeduardotsdev.livrariaapi.security.CustomAuthentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -13,12 +14,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
@@ -27,6 +32,8 @@ import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Configuration
@@ -102,5 +109,25 @@ public class AuthorizationServerConfiguration {
     @Bean
     public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource){
         return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
+
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer(){
+        return context -> {
+            var principal = context.getPrincipal();
+
+            if (principal instanceof CustomAuthentication authentication){
+                OAuth2TokenType tipoToken = context.getTokenType();
+                if (OAuth2TokenType.ACCESS_TOKEN.equals(tipoToken)){
+                    Collection<GrantedAuthority> authorities = authentication.getAuthorities();
+                    List<String> authoritiesList = authorities.stream().map(GrantedAuthority::getAuthority).toList();
+
+                    context
+                            .getClaims()
+                            .claim("authorities", authoritiesList)
+                            .claim("email", authentication.getUsuario().getEmail());
+                }
+            }
+        };
     }
 }
