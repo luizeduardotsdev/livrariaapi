67ecb6ce934f816d1221e832fa890d9d607298aa
luiz <luizeduardotsdev@gmail.com>
2026-01-18 14:19:48 -0300

    docs: documenta controller de client e usuario com respostas e nome

--- src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/ClientController.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/ClientController.java
@@ -2,15 +2,18 @@ package io.github.luizeduardotsdev.livrariaapi.controller;
 
 import io.github.luizeduardotsdev.livrariaapi.controller.dto.ClientDTO;
 import io.github.luizeduardotsdev.livrariaapi.controller.mapper.ClientMapper;
-import io.github.luizeduardotsdev.livrariaapi.model.Client;
 import io.github.luizeduardotsdev.livrariaapi.service.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clients")
+@Tag(name = "Client")
 public class ClientController {
 
     private final ClientService clientService;
@@ -24,6 +27,10 @@ public class ClientController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasRole('GERENTE')")
+    @Operation(summary = "Salvar", description = "Cadastra um novo Client.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Client cadastrado.")
+    })
     public void salvar(@RequestBody ClientDTO clientDTO){
         var client = clientMapper.toEntity(clientDTO);
         clientService.salvar(client);
--- src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/UsuarioController.java
+++ src/main/java/io/github/luizeduardotsdev/livrariaapi/controller/UsuarioController.java
@@ -3,12 +3,17 @@ package io.github.luizeduardotsdev.livrariaapi.controller;
 import io.github.luizeduardotsdev.livrariaapi.controller.dto.UsuarioDTO;
 import io.github.luizeduardotsdev.livrariaapi.controller.mapper.UsuarioMapper;
 import io.github.luizeduardotsdev.livrariaapi.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuarios")
+@Tag(name = "Usuario")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
@@ -21,6 +26,10 @@ public class UsuarioController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Salvar", description = "Cadastra um novo usuario.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Usuario cadastrado.")
+    })
     public void salvar(@RequestBody @Valid UsuarioDTO usuarioDTO) {
         var usuario = usuarioMapper.toEntity(usuarioDTO);
         usuarioService.salvar(usuario);
